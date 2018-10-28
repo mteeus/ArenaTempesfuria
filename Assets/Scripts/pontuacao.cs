ceb8f86f850d05923b6466eb0f583e8c24964357
Matheus Lim <mteeus@gmail.com>
2018-10-28 15:30:15 -0300

    Inicio

---
+++ Assets/Scripts/pontuacao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pontuacao : MonoBehaviour {
+	public static int pontos1;
+	public static int pontos2;
+
+	public Text Score1;
+	public Text Score2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Score1.text = pontos1.ToString();
+		Score2.text = pontos2.ToString();
+	}
+}
