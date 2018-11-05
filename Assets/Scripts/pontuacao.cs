26ae953054c645e5d62d11d3b42d3a146e6aafa3
JeanCarlo1908 <41553588+JeanCarlo1908@users.noreply.github.com>
2018-11-04 22:43:04 -0200

    Update pontuacao.cs
    Criação de função para verificar se o jogador venceu ou não o jogo.

--- Assets/Scripts/pontuacao.cs
+++ Assets/Scripts/pontuacao.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+
 public class pontuacao : MonoBehaviour {
-	public static int pontos1;
-	public static int pontos2;
+    /// <summary>
+    /// Pontuação do Player 1.
+    /// </summary>
+    public static int pontos1;
+    /// <summary>
+    /// Pontuação do Player 2.
+    /// </summary>
+    public static int pontos2;
+    /// <summary>
+    /// Constante com o valor de pontuação que determina o vencedor do jogo
+    /// </summary>
+    public const int PontuacaoMaxima = 3;
 
 	public Text Score1;
 	public Text Score2;
@@ -15,8 +25,37 @@ public class pontuacao : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+    /// <summary>
+    /// Atualização de pontuação
+    /// </summary>
 	void Update () {
+
+        // Atualização do placar.
 		Score1.text = pontos1.ToString();
 		Score2.text = pontos2.ToString();
-	}
+
+        // Verificação se o jogo terminou e quem venceu.
+        if (JogadorVencedor(pontos1))
+        {
+            EditorUtility.DisplayDialog("Fim de Jogo", "O player 1 venceu o jogo!", "Ok");
+        }
+        if (JogadorVencedor(pontos2))
+        {
+            EditorUtility.DisplayDialog("Fim de Jogo", "O player 2 venceu o jogo!", "Ok");
+        }
+    }
+
+    /// <summary>
+    /// Verifica se o jogador foi vencendor. Recebe o valor da pontuação como parâmetro.
+    /// </summary>
+    /// <param name="Pontuacao"> </param>
+    /// <returns></returns>
+    public static bool JogadorVencedor(int Pontuacao)
+    {
+        if (Pontuacao == PontuacaoMaxima)
+        {
+            return true;
+        }
+        return false;
+    }
 }
